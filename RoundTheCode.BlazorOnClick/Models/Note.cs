862b03473457872fa7ef0be3b2f6d9b54082d752
Dave <davidgrace00@gmail.com>
2021-07-04 15:44:12 +0100

    Initial notes application

---
+++ RoundTheCode.BlazorOnClick/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoundTheCode.BlazorOnClick.Models
+{
+    public class Note
+    {
+        public string Message { get; }
+
+        public DateTimeOffset Created { get; }
+
+        public Note(string message)
+        {
+            Message = message;
+            Created = DateTimeOffset.UtcNow;
+        }
+    }
+}
